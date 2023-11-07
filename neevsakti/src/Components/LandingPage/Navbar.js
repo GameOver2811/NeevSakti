abd41cc2dc3bb45168d29b8cafb1f786ce4ee862
Krishna <kdwivedi343@gmail.com>
2023-11-07 20:53:06 +0530

    Fixed navbar at top

--- neevsakti/src/Components/LandingPage/Navbar.js
+++ neevsakti/src/Components/LandingPage/Navbar.js
@@ -44,7 +44,7 @@ export default function Navbar(props) {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" >
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
