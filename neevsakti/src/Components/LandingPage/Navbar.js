2487f55691ae2e4f2921c00255030ab9574a2d4f
Krishna <kdwivedi343@gmail.com>
2023-11-21 22:15:36 +0530

    Wrapping up landing page

--- neevsakti/src/Components/LandingPage/Navbar.js
+++ neevsakti/src/Components/LandingPage/Navbar.js
@@ -44,7 +44,7 @@ export default function Navbar(props) {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" >
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top navbar-dark bg-dark" >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
